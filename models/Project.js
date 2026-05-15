38fa212552eea04c9c00bb5576a7a64ac2e76ec3
Raj <raj123kumar0123@gmail.com>
2026-05-15 10:31:11 +0530

    [Raj] feat: add owner controls to remove session participants

---
+++ models/Project.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  visibility: {
+    type: String,
+    enum: ['Public', 'Private'],
+    default: 'Public',
+  },
+  templateId: {
+    type: Number,
+    default: null, // Can be used later when we have templates
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  forkedFrom: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    default: null,
+  },
+  starCount: {
+    type: Number,
+    default: 0,
+  },
+  forkCount: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true, // Automatically adds createdAt and updatedAt
+});
+
+// Map _id to projectId in JSON responses
+projectSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.projectId = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+const Project = mongoose.model('Project', projectSchema);
+export default Project;
